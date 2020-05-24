b0bc7113a474d1298e30f7ebc0a274ff44cd079c
Kaito Nakanishi <septet.f.d.p@gmail.com>
2020-05-24 15:58:01 +0900

    organize in folders

--- 161-C.py
+++
@@ -1,7 +0,0 @@
-N, K = map(int, input().split())
-
-if(N%2==0):
-    if(K%2==0):
-        print(0)
-else:
-    print(1)
--- 167-A.py
+++
@@ -1,8 +0,0 @@
-S=input()
-T=input()
-
-T_=T[:-1]
-if (S==T_):
-    print("Yes")
-else:
-    print("No")
